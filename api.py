93a63a79c2707775e16526c21a79eedb5f78633b
Clément Galland <clement.galland@epita.fr>
2018-07-01 02:08:59 +0200

    api: remove useless class and add post new

--- api.py
+++ api.py
@@ -7,24 +7,43 @@ from flask import *
 app = Flask(__name__)
 api = Api(app)
 
-class List(Resource):
-    def get(self):
-        conn = create_connection("dabase.db") # connect to database
-        query = conn.execute("select race from dinosaurs")
-        return {'race': query.fetchall()}
+@app.route("/list", methods=["GET"])
+def list():
+    conn = create_connection("dabase.db") # connect to database
+    query = conn.execute("select distinct race from dinosaurs")
+    return jsonify({'races': [i[0] for i in query.fetchall()]})
 
-
-api.add_resource(List, '/list') # Route_1
+@app.route("/new", methods=["POST"])
+def new():
+    conn = create_connection("dabase.db")
+    print(request.get_json())
+    race = request.json["race"]
+    name = request.json["name"]
+    level = request.json["level"]
+    owner = request.json["owner"]
+    tribe = request.json["tribe"]
+    acquired = request.json["acquired"]
+    effectiveness = request.json["effectiveness"]
+    status = request.json["status"]
+    gender = request.json["gender"]
+    health = request.json["health"]
+    stamina = request.json["stamina"]
+    oxygen = request.json["oxygen"]
+    food = request.json["food"]
+    weight = request.json["weight"]
+    damage = request.json["damage"]
+    speed = request.json["speed"]
+    query = conn.execute("insert into dinosaurs values(\
+        '{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}',\
+        '{11}','{12}','{13}','{14}','{15}','{16}')".format(race, name,\
+        owner, trive, acquired, effectiveness, status, gender, health,\
+        stamina, oxygen, food, weight, damage, speed))
+    conn.commit()
+    query = conn.execute("SELECT last_insert_rowid()")
+    return jsonify({ "id" : query.fetchall()})
 
 if __name__ == '__main__':
     conn = create_connection("dabase.db")
     if conn is not None:
         create_table(conn)
-        task = (1, "spino")
-        sql = ''' INSERT INTO dinosaurs(id, race)
-                      VALUES(?,?) '''
-        cur = conn.cursor()
-        cur.execute(sql, task)
-        print(cur.lastrowid)
-        conn.commit()
         app.run(port='5002')
