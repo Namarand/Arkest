d899ebc3cf127c7e4f9263fc65ff9e75d62adbb1
Clément Galland <clement.galland@epita.fr>
2018-07-01 10:50:11 +0200

    api: add get dinosaurs list and get by id

--- api.py
+++ api.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 api = Api(app)
 
 def check(json):
-    if json["acquired"] != "tamed" OR json["acquired"] != "breeded":
+    if json["acquired"] != "tamed" or json["acquired"] != "breeded":
         return "acquired must me 'tamed' or 'breeded'"
-    if json["status"] != "alive" OR json["status"] != "dead":
+    if json["status"] != "alive" or json["status"] != "dead":
         return "status must me 'alive' or 'dead'"
-    if json["gender"] != "male" OR json["gender"] != "female":
+    if json["gender"] != "male" or json["gender"] != "female":
         return "gender must me 'male' or 'female'"
     if json["health"] + json["stamina"] + json["food"] + json["oxygen"] +\
        json["weight"] + json["damage"] + json["speed"] > 450:
@@ -24,6 +24,22 @@ def list():
     conn = create_connection("dabase.db") # connect to database
     query = conn.execute("select distinct race from dinosaurs")
     return jsonify({'races': [i[0] for i in query.fetchall()]})
+
+@app.route("/list/<kind>", methods=["GET"])
+def dinosaurs_get(kind):
+    conn = create_connection("dabase.db")
+    query = conn.execute("select * from dinosaurs where dinosaurs.race = '{0}'".format(kind))
+    return jsonify ({'dinosaurs': query.fetchall()})
+
+@app.route("/dinosaurs/<ident>", methods=["GET"])
+def dinosaurs_get_by_id(ident):
+    conn = create_connection("dabase.db")
+    query = conn.execute("select * from dinosaurs where dinosaurs.id = '{0}'".format(ident))
+    res = query.fetchone()
+    if res is None:
+        return jsonify({'failure': "no such id"})
+    else:
+        return jsonify ({res})
 
 @app.route("/new", methods=["POST"])
 def new():
