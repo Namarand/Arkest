87925acddbc4038b6d4df6d88cde95380756349b
Clément Galland <clement.galland@epita.fr>
2018-07-06 11:39:28 +0200

    view: change view from function to class

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    url(r'^dinosaurs/list/$', views.races_list),
-    url(r'^dinosaurs/list/(?P<kind>[a-zA-Z]+)/$', views.dinosaurs_list),
-    url(r'^dinosaurs/$', views.dinosaurs),
-    url(r'^dinosaurs/(?P<identifier>[0-9]+)/$', views.dinosaurs_by_id),
+    url(r'^dinosaurs/list/$', views.races_list.as_view()),
+    url(r'^dinosaurs/list/(?P<kind>[a-zA-Z]+)/$', views.dinosaurs_list.as_view()),
+    url(r'^dinosaurs/$', views.dinosaurs.as_view()),
+    url(r'^dinosaurs/(?P<identifier>[0-9]+)/$', views.dinosaurs_by_id.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -3,28 +3,26 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.views import APIView
 from api.models import Dinosaur
 from api.serializers import DinosaurSerializer
 
-@csrf_exempt
-def races_list(request):
-    if request.method == 'GET':
+class races_list(APIView):
+    def get(request):
         dinosaurs = Dinosaur.objects.values('race').distinct()
         races = []
         for i in dinosaurs:
             races += [i['race']]
         return JsonResponse({"count" : len(races), "races" : races}, safe=False)
 
-@csrf_exempt
-def dinosaurs_list(request, kind):
-    if request.method == 'GET':
+class dinosaurs_list(APIView):
+    def get(request, kind):
         dinosaurs = Dinosaur.objects.all().filter(race=kind)
         serializer = DinosaurSerializer(dinosaurs, many=True)
         return JsonResponse({"count" : len(dinosaurs), "dinosaurs": serializer.data}, safe=False)
 
-@csrf_exempt
-def dinosaurs(request):
-    if request.method == 'GET':
+class dinosaurs(APIView):
+    def get(self, request):
         data = JSONParser().parse(request)
         results = []
         for i in data["ids"]:
@@ -34,7 +32,7 @@ def dinosaurs(request):
             else:
                 results += {"id": i, "error": "no such dinosaurs"}
         return JsonResponse({"dinosaurs": results})
-    elif request.method == 'POST':
+    def post(self, request):
         data = JSONParser().parse(request)
         serializer = DinosaurSerializer(data=data)
         if serializer.is_valid():
@@ -42,20 +40,24 @@ def dinosaurs(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-@csrf_exempt
-def dinosaurs_by_id(request, identifier):
-    try:
-        dino = Dinosaur.objects.get(pk=identifier)
-    except Dinosaur.DoesNotExist:
-        return JsonResponse({"id": identifier, "error" : "no sush dinosaurs" }, status=400)
-    if request.method == 'PUT':
+class dinosaurs_by_id(APIView):
+    def get_by_id(identifier):
+        try:
+            return Dinosaur.objects.get(pk=identifier)
+        except Dinosaur.DoesNotExist:
+            raise Http404
+
+    def put(request, identifier):
+        dino = get_by_id(identifier)
         data = JSONParser().parse(request)
         serializer = DinosaurSerializer(dino, data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
         return JsonResponse(serializer.errors, status=400)
-    elif request.method == 'DELETE':
+
+    def delete(request, identifier):
+        dino = get_by_id(identifier)
         dino.delete()
         return HttpResponse(status=204)
 
