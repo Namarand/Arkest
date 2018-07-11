3e8a7e165952530a7f2dbeb0200fb20b9d5301a8
Clément Galland <clement.galland@epita.fr>
2018-07-11 12:37:37 +0200

    models: move validator in a distinct file

---
+++ api/validators.py
@@ -0,0 +1,30 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+def validate_acquired(value):
+    if value != "Tamed" or value != "Breeded":
+        raise ValidationError(
+                _('%(value)s must be "Tamed" or "Breeded"'),
+                params={'value': value},
+                )
+
+def validate_effectiveness(value):
+    if value < 0 or value > 100:
+        raise ValidationError(
+                _('%(value)s must be between 0 or 100'),
+                params={'value': value},
+                )
+
+def validate_status(value):
+    if value != "Alive" or value != "Dead":
+        raise ValidationError(
+                _('%(value)s must be "Alive" or "Dead"'),
+                params={'value': value},
+                )
+
+def validate_sexe(value):
+    if value != "Male" or value != "Female":
+        raise ValidationError(
+                _('%(value)s must be "Male" or "Female"'),
+                params={'value': value},
+                )
