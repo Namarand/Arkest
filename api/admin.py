c2c46dfeada813f871099757366dcbb7e409b818
Clément Galland <clement.galland@epita.fr>
2018-07-11 11:36:58 +0200

    group: add apiViewer group

--- api/admin.py
+++ api/admin.py
@@ -1,13 +1,8 @@
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.contrib import admin
 from api.models import Dinosaur
 
-group, other = Group.objects.get_or_create(name="ApiUser")
-
-content_type = ContentType.objects.get_for_model(model=Dinosaur)
-#get all permssions for this model
-perms = Permission.objects.filter(content_type=content_type)
-
-for p in perms:
-    group.permissions.add(p)
-#group = Group.objects.get(name="ApiUser")
+Group.objects.get_or_create(name="ApiUser")
+Group.objects.get_or_create(name="ApiViewer")
+admin.site.register(Dinosaur)
