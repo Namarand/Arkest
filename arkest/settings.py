b392030c7ba3ae2d489aa0040b711a6cc4cf7375
Clément Galland <clement.galland@epita.fr>
2018-07-11 11:37:34 +0200

    group: remove default auth

--- arkest/settings.py
+++ arkest/settings.py
@@ -53,9 +53,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-            'rest_framework.permissions.IsAdminUser',
-            ),
+#    'DEFAULT_PERMISSION_CLASSES': (
+#            'rest_framework.permissions.IsAdminUser',
+#            ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
             'rest_framework.authentication.SessionAuthentication',
