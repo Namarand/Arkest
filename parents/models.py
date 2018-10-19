d445d741ba9a3ac7b94eea056d285fe2b59134dd
Clément Galland <clement.galland@epita.fr>
2018-10-19 10:31:27 +0200

    parents: change attribute name to xxx_id when referencing to a ForeignKey

--- parents/models.py
+++ parents/models.py
@@ -5,9 +5,9 @@ from parents.validators import *
 
 # Create your models here.
 class Parents(models.Model):
-    child = models.ForeignKey(Dinosaur, on_delete=models.CASCADE, related_name='child')
-    father = models.ForeignKey(Dinosaur, on_delete=models.SET_NULL, null=True, related_name='father')#, validators=[validate_father])
-    mother = models.ForeignKey(Dinosaur, on_delete=models.SET_NULL, null=True, related_name='mother')#, validators=[validate_mother])
+    child_id = models.ForeignKey(Dinosaur, on_delete=models.CASCADE, related_name='child_id')
+    father_id = models.ForeignKey(Dinosaur, on_delete=models.SET_NULL, null=True, related_name='father_id', validators=[validate_father])
+    mother_id = models.ForeignKey(Dinosaur, on_delete=models.SET_NULL, null=True, related_name='mother_id', validators=[validate_mother])
 
     class Meta:
-        ordering = ('child',)
+        ordering = ('child_id',)
